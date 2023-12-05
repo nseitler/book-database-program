a70aa08cc25afea0b28c5001516083fae504d355
nseitler <hello@nickseitler.com>
2023-12-05 13:48:18 -0500

    added analysis

--- app.py
+++ app.py
@@ -23,6 +23,21 @@ def menu():
                   \rA number from 1-5.
                   \rPress enter to try again.''')
 
+def submenu():
+    while True:
+            print('''
+                \r1) Edit
+                \r2) Delete
+                \r3) Return to main menu''')
+            choice = input('What would you like to do?   ')
+            if choice in ['1', '2', '3']:
+                return choice
+            else:
+                input('''
+                    \rPlease choose one of the options above.
+                    \rA number from 1-3.
+                    \rPress enter to try again.''')
+
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June', 
               'July', 'August', 'September', 'October', 'November', 'December']
@@ -77,6 +92,31 @@ def clean_id(id_str, options):
               \rPress enter to try again
               \r***********************''')
             return
+
+def edit_check(column_name, current_value):
+    print(f'\n**** EDIT {column_name} ****')
+    if column_name == 'Price':
+        print(f'\rCurrent Value: {current_value/100}')
+    elif column_name == 'Date':
+        print(f'\rCurrent Value: {current_value.strftime("%B %d, %Y")}')
+    else:
+        print(f'\rCurrent Value: {current_value}')
+    
+    if column_name =='Date' or column_name == 'Price':
+        while True:
+            changes = input('What would you like to change the value to? ')
+            if column_name == 'Date':
+                changes = clean_date(changes)
+                if type (changes) == datetime.date:
+                    return changes
+            elif column_name == 'Price':
+                changes = clean_price(changes)
+                if type(changes) == int:
+                    return changes
+                    
+    else:
+        return input('What would you like to change the value to? ')
+
 
 def add_csv():
     with open('suggested_books.csv') as csvfile:
@@ -140,10 +180,34 @@ def app():
                   \n{the_book.title} by {the_book.author}
                   \rPublished: {the_book.published_date}
                   \rPrice: ${the_book.price / 100}''')
-            input('\nPress enter to return to the main menu')
+            sub_choice = submenu()
+            if sub_choice == '1':
+                # edit
+                the_book.title = edit_check('Title', the_book.title)
+                the_book.author = edit_check('Author', the_book.author)
+                the_book.published_date = edit_check('Date', the_book.published_date)
+                the_book.price = edit_check('Price', the_book.price)
+                session.commit()
+                print('Book updated!')
+                time.sleep(1.5)
+            elif sub_choice =='2':
+                # delete
+                session.delete(the_book)
+                session.commit()
+                print('Book deleted!')
+                time.sleep(1.5)
         elif choice == '4':
             # analysis
-            pass
+            oldest_book = session.query(Book).order_by(Book.published_date).first()
+            newest_book = session.query(Book).order_by(Book.published_date.desc()).first()
+            total_books = session.query(Book).count()
+            python_books = session.query(Book).filter(Book.title.like('%Python%')).count()
+            print(f'''\n***** BOOK ANALYSIS *****
+                  \rOldest Book: {oldest_book.title}
+                  \rNewest Book: {newest_book.title}
+                  \rTotal Books: {total_books}
+                  \rNumber of Python Books: {python_books}''')
+            input('\nPress enter to return to the main menu.')
         else:
             print('GOODBYE')
             app_running = False
